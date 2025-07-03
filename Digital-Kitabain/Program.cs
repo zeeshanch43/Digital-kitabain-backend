df1dbedb812dec11ef412f8847b0f94e5f601afd
rayyan2907 <mrayyan5296@gmail.com>
2025-07-03 23:00:54 +0500

    swagger added

--- Digital-Kitabain/Program.cs
+++ Digital-Kitabain/Program.cs
@@ -7,15 +7,19 @@ DotNetEnv.Env.Load();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
